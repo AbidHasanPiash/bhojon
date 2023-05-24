44d8fc2516959752558b4901a7037be811e3419f
Abid <meghpiash@gmail.com>
2023-05-24 11:54:02 +0600

    nab bar setup for routing

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,9 +4,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { SidebarContext } from '@/context/SidebarContext'
 
-// React Icons
+// Icons
 import {HiOutlineLogout} from 'react-icons/hi'
 import {ImCross} from 'react-icons/im'
+import DashboardSVG from './svg/DashboardSVG'
+import MenuSVG from './svg/MenuSVG'
+import OrderSVG from './svg/OrderSVG'
+import FinanceSVG from './svg/FinanceSVG'
+import EmployeeSVG from './svg/EmployeeSVG'
 
 export default function nav() {
   const {isSidebarOpen, setSidebarOpen} = useContext(SidebarContext);
@@ -41,121 +46,46 @@ export default function nav() {
             <Link
               href="#"
               aria-label="dashboard"
-              className="relative flex items-center space-x-4 rounded-xl bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white"
+              className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300 active:bg-gradient-to-r from-sky-600 to-cyan-400"
             >
-              <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
-                <path
-                  d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
-                  className="dark:fill-slate-600 fill-current text-cyan-400"
-                ></path>
-                <path
-                  d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
-                  className="fill-current text-cyan-200 group-hover:text-cyan-300"
-                ></path>
-                <path
-                  d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
-                  className="fill-current group-hover:text-sky-300"
-                ></path>
-              </svg>
-              <span className="-mr-1 font-medium">Dashboard</span>
+              <DashboardSVG/>
+              <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">Dashboard</span>
             </Link>
           </li>
           <li>
             <Link
               href="#"
-              className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300"
+              className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300 active:bg-gradient-to-r from-sky-600 to-cyan-400"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  className="fill-current text-gray-300 group-hover:text-cyan-300"
-                  fill-rule="evenodd"
-                  d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
-                  clip-rule="evenodd"
-                />
-                <path
-                  className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-sky-400"
-                  d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z"
-                />
-              </svg>
-              <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">Categories</span>
+              <MenuSVG/>
+              <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">Menu</span>
             </Link>
           </li>
           <li>
             <Link
               href="#"
-              className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300"
+              className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300 active:bg-gradient-to-r from-sky-600 to-cyan-400"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-sky-400"
-                  fill-rule="evenodd"
-                  d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                  clip-rule="evenodd"
-                />
-                <path
-                  className="fill-current text-gray-300 group-hover:text-cyan-300"
-                  d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"
-                />
-              </svg>
-              <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">Reports</span>
+              <OrderSVG/>
+              <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">Order</span>
             </Link>
           </li>
           <li>
             <Link
               href="#"
-              className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300"
+              className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300 active:bg-gradient-to-r from-sky-600 to-cyan-400"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-cyan-400"
-                  d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"
-                />
-                <path
-                  className="fill-current text-gray-300 group-hover:text-cyan-300"
-                  d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"
-                />
-              </svg>
-              <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">Other data</span>
+              <FinanceSVG/>
+              <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">Finance</span>
             </Link>
           </li>
           <li>
             <Link
               href="#"
-              className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300"
+              className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300 active:bg-gradient-to-r from-sky-600 to-cyan-400"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  className="fill-current text-gray-300 group-hover:text-cyan-300"
-                  d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"
-                />
-                <path
-                  className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-sky-400"
-                  fill-rule="evenodd"
-                  d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span className="group-hover:text-gray-700 dark:group-hover:text-white">Finance</span>
+              <EmployeeSVG/>
+              <span className="group-hover:text-gray-700 dark:group-hover:text-white">Employee</span>
             </Link>
           </li>
         </ul>
---
+++ components/svg/FinanceSVG.jsx
@@ -0,0 +1,14 @@
+export default function FinanceSVG() {
+  return (
+    <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+      <path
+        className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-cyan-400"
+        d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"
+      />
+      <path
+        className="fill-current text-gray-300 group-hover:text-cyan-300"
+        d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"
+      />
+    </svg>
+  );
+}
