ecd867d2a99c94df19d169b987eafec72f0a599f
Abid <meghpiash@gmail.com>
2023-06-24 22:13:31 +0600

    receipt modal creating

--- components/dashboard/ReceiptModal.jsx
+++ components/dashboard/ReceiptModal.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+"use client"
+import { useState } from "react";
 import { ImCross } from "react-icons/im";
 
 export default function ReceiptModal({closeReceiptModal}) {
+  const [selectedOrder, setSelectedOrder] = useState('');
+  const Orders = [
+    {id:2360005, table:7},
+    {id:2360004, table:2},
+    {id:2360003, table:5},
+    {id:2360002, table:6},
+    {id:2360001, table:1},
+  ]
   return (
     <div className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black/10 backdrop-blur-md flex items-center justify-center w-full h-full z-30`}>
-      <div className="w-fit h-fit space-y-3 mx-6 p-4 sm:p-8 rounded-3xl bg-white border border-gray-200/50 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
+      <div className="min-w-fit h-fit space-y-3 mx-6 p-4 sm:p-8 rounded-3xl bg-white border border-gray-200/50 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
         {/* Modal Header */}
-        <div className='w-full flex items-center justify-between'>
-          <h1 className='text-xl md:text-2xl'>Case Receipt</h1>
+        <div className='w-full flex items-center justify-between space-x-10'>
+          <h1 className='text-lg md:text-xl'>Case Receipt</h1>
           <button
             onClick={closeReceiptModal}
             aria-label="Close Nav"
@@ -17,8 +26,25 @@ export default function ReceiptModal({closeReceiptModal}) {
           </button>
         </div>
         {/* Modal Body */}
-        <div>
-          modal
+        <div className="grid grid-cols-2">
+          <div>
+            items
+            <select
+              value={selectedOrder}
+              onChange={(e)=>setSelectedOrder(e.target.value)}
+              className="outline-none w-full rounded-xl border border-gray-300 p-2.5 text-sm transition-all duration-300 dark:bg-gray-900 dark:border-gray-700"
+            >
+              <option value="">Select a category</option>
+              {Orders.map((Order, index) => (
+                <option key={index} value={Order.id}>
+                  {Order.id} / {Order.table}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            cash
+          </div>
         </div>
       </div>
     </div>
