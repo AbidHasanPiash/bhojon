5cc415427a9b2633115acc942ed56c4e980e3b80
Abid <meghpiash@gmail.com>
2023-08-14 00:34:21 +0600

    meta added

--- app/layout.js
+++ app/layout.js
@@ -8,12 +8,25 @@ import { useState } from 'react'
 import { usePathname  } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] })
+export const metadata = {
+  title: 'Resto Man.',
+  description: 'our Digital Restaurant Solution.',
+}
 
 export default function RootLayout({ children }) {
   const path = usePathname();
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        
+        {/* Open Graph tags for social sharing */}
+        <meta property="og:title" content="RestoMan: Restaurant Management System" />
+        <meta property="og:description" content="Effortlessly streamline restaurant operations, 
+        manage customer insights, and drive growth with RestoMan's intuitive digital solutions." />
+        <meta property="og:image" content="/images/logo.png" />
+      </head>
       <body className={`${inter.className} body_and_text_color`}>
         <SidebarContext.Provider value={{isSidebarOpen, setSidebarOpen}}>
           {path !== '/' && <Nav/>}
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Features from "@/components/home/Features";
 import WhyRestoMan from "@/components/home/WhyRestoMan";
 import Footer from "@/components/home/Footer";
@@ -11,17 +10,6 @@ import Testimonials from "@/components/home/Testimonials";
 export default function Home() {
   return (
     <div className="!scroll-smooth">
-      <Head>
-        <title>Resto Man.</title>
-        <meta name="description" content="Your Digital Restaurant Solution." />
-        
-        {/* Open Graph tags for social sharing */}
-        <meta property="og:title" content="RestoMan: Restaurant Management System" />
-        <meta property="og:description" content="Effortlessly streamline restaurant operations, 
-        manage customer insights, and drive growth with RestoMan's intuitive digital solutions." />
-        <meta property="og:image" content="/images/logo.png" />
-        {/* Add more Open Graph tags as needed */}
-      </Head>
       <Nav />
       <section id="login"> <Login /> </section>
       <section id="why"> <WhyRestoMan /> </section>
