7eb02dbe7133fa6d29a1e031df6399abb5a0d466
Abid <meghpiash@gmail.com>
2023-08-14 22:34:12 +0600

    changed image for purchase page

--- app/purchase/page.jsx
+++ app/purchase/page.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
@@ -6,71 +7,52 @@ export default function page() {
         <div className="py-16">
             <div className="container m-auto space-y-8 px-6 text-gray-500 md:px-12 lg:px-20">
                 <div className="flex items-center justify-center -space-x-2">
-                <img
-                    loading="lazy"
-                    width="220"
-                    height="220"
-                    src="https://tailus.io/sources/blocks/app/preview/images/avatars/second_user.webp"
-                    alt="member photo"
-                    className="h-8 w-8 rounded-full object-cover"
-                />
-                <img
-                    loading="lazy"
-                    width="220"
-                    height="220"
-                    src="https://tailus.io/sources/blocks/app/preview/images/avatars/first_user.webp"
-                    alt="member photo"
-                    className="h-12 w-12 rounded-full object-cover"
-                />
-                <img
-                    loading="lazy"
-                    width="220"
-                    height="220"
-                    src="https://tailus.io/sources/blocks/app/preview/images/avatars/third_user.webp"
-                    alt="member photo"
-                    className="z-10 h-16 w-16 rounded-full object-cover"
-                />
-                <img
-                    loading="lazy"
-                    width="220"
-                    height="220"
-                    src="https://tailus.io/sources/blocks/app/preview/images/avatars/first_user.webp"
-                    alt="member photo"
-                    className="relative h-12 w-12 rounded-full object-cover"
-                />
-                <img
-                    loading="lazy"
-                    width="220"
-                    height="220"
-                    src="https://tailus.io/sources/blocks/app/preview/images/avatars/second_user.webp"
-                    alt="member photo"
-                    className="h-8 w-8 rounded-full object-cover"
-                />
+                    <img
+                        loading="lazy"
+                        width="220"
+                        height="220"
+                        src="https://tailus.io/sources/blocks/app/preview/images/avatars/second_user.webp"
+                        alt="member photo"
+                        className="h-8 w-8 rounded-full object-cover"
+                    />
+                    <img
+                        loading="lazy"
+                        width="220"
+                        height="220"
+                        src="https://tailus.io/sources/blocks/app/preview/images/avatars/first_user.webp"
+                        alt="member photo"
+                        className="h-12 w-12 rounded-full object-cover"
+                    />
+                    <img
+                        loading="lazy"
+                        width="220"
+                        height="220"
+                        src="https://tailus.io/sources/blocks/app/preview/images/avatars/third_user.webp"
+                        alt="member photo"
+                        className="z-10 h-16 w-16 rounded-full object-cover"
+                    />
+                    <img
+                        loading="lazy"
+                        width="220"
+                        height="220"
+                        src="https://tailus.io/sources/blocks/app/preview/images/avatars/first_user.webp"
+                        alt="member photo"
+                        className="relative h-12 w-12 rounded-full object-cover"
+                    />
+                    <img
+                        loading="lazy"
+                        width="220"
+                        height="220"
+                        src="https://tailus.io/sources/blocks/app/preview/images/avatars/second_user.webp"
+                        alt="member photo"
+                        className="h-8 w-8 rounded-full object-cover"
+                    />
                 </div>
                 <div className="m-auto space-y-6 md:w-8/12 lg:w-7/12">
-                <h1 className="text-center text-4xl font-bold text-gray-800 dark:text-white md:text-5xl">Get Started now</h1>
-                <p className="text-center text-xl text-gray-600 dark:text-gray-300">
-                    Be part of millions people around the world using tailus in modern User Interfaces.
-                </p>
-                <div className="flex flex-wrap justify-center gap-6">
-                    <a
-                        href="#"
-                        className="relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
-                    >
-                        <span className="relative text-base font-semibold text-white dark:text-dark"
-                        >Get Started</span
-                        >
-                    </a>
-                    <a
-                        href="#"
-                        className="relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:border before:border-gray-200 before:bg-gray-50 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
-                    >
-                        <span
-                        className="relative text-base font-semibold text-primary dark:text-white"
-                        >More about</span
-                        >
-                    </a>
-                </div>
+                    <h1 className="text-center text-4xl font-bold text-gray-800 dark:text-white md:text-5xl">Get Started now</h1>
+                    <p className="text-center text-xl text-gray-600 dark:text-gray-300">
+                        Embark on a journey to elevate your restaurant's success. <br /> Join Resto Man today.
+                    </p>
                 </div>
             </div>
         </div>
@@ -110,7 +92,7 @@ export default function page() {
                 <div className="grid grid-cols-5 grid-rows-4 gap-4 md:w-5/12 lg:w-6/12">
                     <div className="col-span-2 row-span-4">
                     <img
-                        src="https://tailus.io/sources/blocks/ecommerce-site/preview/images/products/kushagra.webp"
+                        src="/images/common/demo1.png"
                         className="rounded-full"
                         width="640"
                         height="960"
@@ -120,7 +102,7 @@ export default function page() {
                     </div>
                     <div className="col-span-2 row-span-2">
                     <img
-                        src="https://tailus.io/sources/blocks/ecommerce-site/preview/images/products/iman.webp"
+                        src="/images/common/demo3.png"
                         className="h-full w-full rounded-xl object-cover object-top"
                         width="640"
                         height="640"
@@ -130,7 +112,7 @@ export default function page() {
                     </div>
                     <div className="col-span-3 row-span-3">
                     <img
-                        src="https://tailus.io/sources/blocks/ecommerce-site/preview/images/products/daniel.webp"
+                        src="/images/common/demo2.png"
                         className="h-full w-full rounded-xl object-cover object-top"
                         width="640"
                         height="427"
