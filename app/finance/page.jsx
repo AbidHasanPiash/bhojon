fef928c9da5d53cbaa7d6f4e576dc5f5c0c5cdb9
Abid <meghpiash@gmail.com>
2023-08-17 01:04:27 +0600

    date picker added

--- app/finance/page.jsx
+++ app/finance/page.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import ExpenseRow from '@/components/finance/ExpenseRow';
 import { HiPlusCircle, HiSave } from 'react-icons/hi';
+import Report from '@/components/finance/Report';
 
 export default function Finance() {
   // Date relate
@@ -15,6 +16,7 @@ export default function Finance() {
   const [dailySales, setDailySales] = useState(50);
   const [closing, setClosing] = useState(opening + dailySales);
   const [totalExpenses, setTotalExpenses] = useState(0)
+  // Operational functions
   const handleInputChange = (index, field, value) => {
     const newExpenses = [...expenses];
     newExpenses[index][field] = value;
@@ -47,64 +49,76 @@ export default function Finance() {
   return (
     <div className='container_gap'>
       <div className='flex flex-col-reverse lg:grid grid-cols-4 gap-6 w-full'>
-        <div className='container_layout lg:col-span-3'>
-          {/* Header */}
-          <div className='flex items-center space-x-6 text-xl font-bold md:text-3xl'>
-            <h1>Balance Inquiry.</h1>
-            <h1 className='input_layout2 w-fit text-center'>{currentDate}</h1>
+        <div className='lg:col-span-3 space-y-6'>
+          <div className='container_layout2'>
+            {/* Header */}
+            <div className='flex items-center space-x-6 text-xl font-bold md:text-3xl'>
+              <h1>Balance Inquiry.</h1>
+              <h1 className='input_layout2 w-fit text-center'>{currentDate}</h1>
+            </div>
+            {/* Costing and liability */}
+            <div className='modal_body_element space-y-2'>
+              <h1 className='text-sm text-center text-gray-500 dark:text-gray-300'>Expenditures Breakdown.</h1>
+              {expenses.map((expense, index) => (
+                <ExpenseRow
+                  key={index}
+                  index={index}
+                  category={expense.category}
+                  description={expense.description}
+                  amount={expense.amount}
+                  onChange={handleInputChange}
+                  onRemove={handleRemoveInput}
+                />
+              ))}
+            </div>
+            <div className='modal_body_element flex items-center justify-end space-x-3'>
+              <span className='input_layout2 w-28 text-center'>{totalExpenses}</span>
+              <button className="btn_layout_text" onClick={handleAddInput}>
+                <span className="btn_text"><HiPlusCircle size={22}/> <span>Add</span> </span>
+              </button>
+              <button className="btn_layout_text" onClick={handleSaveData}>
+                <span className="btn_text"><HiSave size={22}/> <span>Save</span> </span>
+              </button>
+            </div>
           </div>
-          {/* Costing and liability */}
-          <div className='modal_body_element space-y-2'>
-            <h1 className='text-sm text-center text-gray-500 dark:text-gray-300'>Expenditures Breakdown.</h1>
-            {expenses.map((expense, index) => (
-              <ExpenseRow
-                key={index}
-                index={index}
-                category={expense.category}
-                description={expense.description}
-                amount={expense.amount}
-                onChange={handleInputChange}
-                onRemove={handleRemoveInput}
-              />
-            ))}
-          </div>
-          <div className='modal_body_element flex items-center justify-end space-x-3'>
-            <span className='input_layout2 w-28 text-center'>{totalExpenses}</span>
-            <button className="btn_layout_text" onClick={handleAddInput}>
-              <span className="btn_text"><HiPlusCircle size={22}/> <span>Add</span> </span>
-            </button>
-            <button className="btn_layout_text" onClick={handleSaveData}>
-              <span className="btn_text"><HiSave size={22}/> <span>Save</span> </span>
-            </button>
-          </div>
-        </div>
-        <div className='container_layout2 space-y-6'>
-          <h1 className='text-xl font-bold md:text-3xl'>Summary.</h1>
-          <div className='grid lg:grid-cols-1 grid-cols-2 gap-3'>
-            <div>
-              <p className='p-1'>Opening</p>
-              <p className='input_layout'>{opening}</p>
-            </div>
-            <div>
-              <p className='p-1'>Invest</p>
-              <input 
-                type="number" 
-                placeholder='Invest' 
-                value={invest}
-                onChange={handleInvest} 
-                className='input_layout' 
-              />
-            </div>
-            <div>
-              <p className='p-1'>Daily Sales</p>
-              <p className='input_layout'>{dailySales}</p>
-            </div>
-            <div>
-              <p className='p-1'>Closing</p>
-              <p className='input_layout'>{closing}</p>
-            </div>
+          <div className='container_layout2'>
+            <Report/>
           </div>
         </div>
+        <div className='space-y-6'>
+          <div className='container_layout2 space-y-6'>
+            <h1 className='text-xl font-bold md:text-3xl'>Summary.</h1>
+            <div className='grid lg:grid-cols-1 grid-cols-2 gap-3'>
+              <div>
+                <p className='p-1'>Opening</p>
+                <p className='input_layout'>{opening}</p>
+              </div>
+              <div>
+                <p className='p-1'>Invest</p>
+                <input 
+                  type="number" 
+                  placeholder='Invest' 
+                  value={invest}
+                  onChange={handleInvest} 
+                  className='input_layout' 
+                />
+              </div>
+              <div>
+                <p className='p-1'>Daily Sales</p>
+                <p className='input_layout'>{dailySales}</p>
+              </div>
+              <div>
+                <p className='p-1'>Closing</p>
+                <p className='input_layout'>{closing}</p>
+              </div>
+            </div>
+          </div>
+          <div className='container_layout2 space-y-6'>
+            expence category
+          </div>
+        </div>
+      </div>
+      <div className='lg:grid grid-cols-4 gap-6 w-full'>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    //...
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   theme: {
     extend: {
