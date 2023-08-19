5d85e1e3016248b8bbcf4995da2ae0e9b761d7b9
Abid <meghpiash@gmail.com>
2023-08-19 23:41:05 +0600

    date filter

--- components/finance/Report.jsx
+++ components/finance/Report.jsx
@@ -6,51 +6,21 @@ import TableRow from "./TableRow";
 export default function Report() {
     // Date relate
     const date = new Date();
-    const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
-    const currentDate = date.toLocaleString('en-US', options);
+    const currentDate = date.toISOString().split('T')[0];
 
     const [selectedDate, setSelectedDate] = useState({ startDate:null, endDate:null }); 
     const handleValueChange = (newValue) => {
         setSelectedDate(newValue);
     } 
-    const [Orders, setOrders] = useState([
-      {id:1, table:3, status: 'active', waiter:'John', time:'11:24 AM', amount:350, items:['Bruschetta', 'Caprese Salad', 'Chicken Wings']},
-      {id:2, table:5, status: 'complete', waiter:'Walle', time:'11:00 AM', amount:465, items:['Grilled Salmon', 'dring']},
-      {id:3, table:1, status: 'active', waiter:'John', time:'10:30 AM', amount:180, items:['chicken', 'dring', 'Grilled Salmon']},
-      {id:4, table:7, status: 'complete', waiter:'Mike', time:'10:12 AM', amount:290, items:['Chocolate Cake', 'Bruschetta']},
-      {id:5, table:10, status: 'active', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'active', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'active', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:5, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-      {id:55, table:10, status: 'complete', waiter:'Jack', time:'09:45 AM', amount:340, items:['Tiramisu', 'dring']},
-    ])
+    const [reports, setReports] = useState([
+      {id:1, date:'2023-08-19', opening: 500, sales:246, expence:180, closing:566, remarks:'N/A'},
+      {id:1, date:'2023-08-18', opening: 500, sales:246, expence:180, closing:566, remarks:'N/A'},
+      {id:1, date:'2023-08-17', opening: 500, sales:246, expence:180, closing:566, remarks:'N/A'},
+      {id:1, date:'2023-08-16', opening: 500, sales:246, expence:180, closing:566, remarks:'N/A'},
+      {id:1, date:'2023-08-15', opening: 500, sales:246, expence:180, closing:566, remarks:'N/A'},
+      {id:1, date:'2023-08-14', opening: 500, sales:246, expence:180, closing:566, remarks:'N/A'},
+    ]);
+    const filtered = reports.filter(report => report.date >= selectedDate.startDate && report.date <= selectedDate.endDate);
   return (
     <div className="space-y-6">
         {/* Header */}
@@ -62,7 +32,7 @@ export default function Report() {
                     placeholder={"Select date"}
                     onChange={handleValueChange}
                     useRange={false} 
-                    displayFormat={"DD-MM-YYYY"}
+                    //displayFormat={"DD-MM-YYYY"}
                     minDate={new Date("2023/08/05")} 
                     maxDate={new Date(currentDate)}
                     inputClassName="px-6 mr-8 outline-none p-0 bg-transparent text-gray-800 dark:text-gray-300 cursor-pointer select-none"   
@@ -75,17 +45,17 @@ export default function Report() {
               <tr className='bg-cyan-200 dark:bg-cyan-800'>
                 <th className='py-2 text-left pl-2 rounded-tl-lg w-16'>SN</th>
                 <th className='py-2 text-left'>Date</th>
-                <th className='py-2 text-left'>Created by</th>
                 <th className='py-2 text-left'>Opening</th>
-                <th className='py-2 text-left'>Daily Sales</th>
+                <th className='py-2 text-left'>Sales</th>
+                <th className='py-2 text-left'>Expence</th>
                 <th className='py-2 text-left'>Closing</th>
                 <th className='py-2 text-left'>Remarks</th>
                 <th className='py-2 text-left rounded-tr-lg'>Action</th>
               </tr>
             </thead>
             <tbody>
-              {Orders.map((order, index)=>(
-                <TableRow key={index} index={index} row={order} Orders={Orders}/>
+              {filtered.map((order, index)=>(
+                <TableRow key={index} index={index} row={order} reports={reports}/>
               ))}
             </tbody>
           </table>
