8a86c541c17a3b83d54968a68de97d786a622ce4
Abid <meghpiash@gmail.com>
2023-09-05 11:52:59 +0600

    card design ok

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
 'use client'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Nav from '@/components/common/Nav'
-import Header from '@/components/common/Header'
-import { SidebarContext } from '@/context/SidebarContext'
-import useSystemTheme from '@/hooks/useSystemTheme'
-import { useState } from 'react'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Nav from '@/components/common/Nav';
+import Header from '@/components/common/Header';
+import { SidebarContext } from '@/context/SidebarContext';
+import useSystemTheme from '@/hooks/useSystemTheme';
+import { useState } from 'react';
 import { usePathname  } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,13 +19,12 @@ export default function RootLayout({ children }) {
     <html className="scroll-smooth" lang="en">
       <head>
         <title>Resto Man.</title>
-        
         {/* Open Graph tags for social sharing */}
         <meta property="og:title" content="RestoMan: Restaurant Management System" />
         <meta property="og:description" content="The ultimate digital restaurant solution, simplifying operations, enhancing efficiency, and elevating dining experiences seamlessly." />
         <meta property="og:image" content="/logos/rm_main.png" />
       </head>
-      <body className={`${inter.className} ${scrollbar} body_and_text_color`}>
+      <body className={` ${inter.className} ${scrollbar} body_and_text_color`}>
         <SidebarContext.Provider value={{isSidebarOpen, setSidebarOpen}}>
           {path !== '/' && path !== '/purchase' && <Nav/>}
           <div className={path !== '/' &&  path !== '/purchase' && 'ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]'}>
--- components/common/TestimonialCard.jsx
+++ components/common/TestimonialCard.jsx
@@ -1,33 +1,39 @@
 import { HiStar } from "react-icons/hi";
 import Image from "next/image";
+import { ImQuotesLeft } from "react-icons/im";
 
-export default function TestimonialCard({info, index}) {
-  let colors = ['bg-green-300', 'bg-amber-300', 'bg-rose-300', 'bg-fuchsia-300', 'bg-cyan-300'];
+export default function TestimonialCard({info}) {
   let stars = new Array;
   for (let index = 0; index < info.star; index++) {
     stars[index]=<HiStar />
   }
   return (
-    <div className={`w-full h-full rounded-2xl text-gray-900 ${colors[index]}`}>
+    <div className={`w-full h-full rounded-2xl text-gray-900 bg-[url('/images/common/card.png')] bg-contain bg-no-repeat`}>
       <div className="w-full h-full flex-col items-center justify-center">
         {/* Upper section of card */}
-        <div className="w-full h-3/4 flex flex-col items-center justify-between">
+        <div className="w-full h-4/6 flex flex-col items-center justify-between">
           <div className="flex items-center justify-start w-full gap-2 p-3">
             <Image src={info.img} width={40} height={40} alt="client" className="rounded-full"/>
-            <h1>{info.name}</h1>
+            <h1 className="font-bold">{info.name}</h1>
           </div>
-          <h1 className=" text-xl lg:text-2xl text-center"> " {info.testimonial} "</h1>
-          <h1 className="p-2 text-sm lg:text-base">{info.restaurant}</h1>
+          <div className="w-full h-full flex items-center justify-center relative">
+            <span className="text-3xl absolute top-6 left-4 text-fuchsia-700"><ImQuotesLeft/></span>
+            <h1 className="font-bold px-2 text-center"> {info.testimonial} </h1>
+            <span className="text-3xl absolute bottom-6 right-4 text-fuchsia-700 rotate-180"><ImQuotesLeft/></span>
+          </div>
         </div>
         {/* Down section of card */}
-        <div className="w-full h-1/4 border-t-2 border-dashed border-gray-900">
-          <div className="flex items-center justify-center py-0.5 space-x-2">
+        <div className="w-full h-2/6 border-t-2 border-dashed border-gray-900">
+          <div className="flex items-center justify-between py-2">
             <span className="px-2 text-xs flex items-center justify-center">{stars}</span>
-            <span className="px-2 text-xs">{info.date}</span>
+            <span className="px-2 text-xs font-bold">{info.date}</span>
+          </div>
+          <div className="text-center font-bold">
+            <h1 className="p-2 text-sm lg:text-base">{info.restaurant}</h1>
           </div>
           <div className="flex-col items-center justify-center w-full">
-            <Image src={"/images/common/barcode.png"} width={400} height={100} alt="code" className="w-4/6 mx-auto opacity-80"/>
-            <p className="text-center text-xs text-black"> 1 8 9 5 6 2 4 8 7 </p>
+            <Image src={"/images/common/barcode.png"} width={400} height={100} alt="code" className="w-5/6 mx-auto opacity-75"/>
+            <p className="text-center text-xs font-bold tracking-widest text-black"> 1 8 9 5 6 2 4 8 7 </p>
           </div>
         </div>
       </div>
